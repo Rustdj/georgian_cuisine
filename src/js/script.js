7d0f339d8cb660fb64196413cd80e15d6c8dd209
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-10-26 10:02:14 +0300

    create tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,7 @@
-/*const hamburger = document.querySelector('.hamburger'),
-menu = document.querySelector('.menu'),
-closeElem = document.querySelector('.menu');
-
-
-hamburger.addEventListener('click', () => {
-menu.classList.add('active');
-});
-
-
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-});*/
-
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu'),
-    hamburger = document.querySelector('.hamburger');
+          menuItem = document.querySelectorAll('.menu'),
+          hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger__active');
@@ -27,5 +13,36 @@ window.addEventListener('DOMContentLoaded', () => {
             hamburger.classList.toggle('hamburger__active');
             menu.classList.toggle('menu__active');
         })
-    })
+    })  
 })
+
+
+// Tabs
+
+const tabsBtn = document.querySelectorAll('.header__btn');
+const tabsItem = document.querySelectorAll('.market__wrapper');
+
+
+tabsBtn.forEach(onTabClick); 
+
+function onTabClick (item) {
+    item.addEventListener( 'click', () => {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId);
+
+        tabsBtn.forEach( (item) => {
+            item.classList.remove('active');
+        });
+
+        tabsItem.forEach( (item) => {
+            item.classList.remove('active');
+        });
+
+        currentBtn.classList.add('active');
+        currentTab.classList.add('active');
+        
+    });
+}
+
+document.querySelector('.header__btn').click();
